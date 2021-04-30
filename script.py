b990cffa5fea059bccc10d5810659402a68e6732
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2021-04-30 11:20:29 +0100

    Add sample data and full test

--- script.py
+++ script.py
@@ -1,3 +1,20 @@
 import subprocess
+from pathlib import Path
 
-subprocess.call('gdal_rasterize')
+outputs = Path("/data/outputs")
+outputs.mkdir(exist_ok=True)
+
+subprocess.call(['gdal_rasterize',
+                 '-burn', '1',
+                 '-tr', '1', '1',
+                 '-co', 'COMPRESS=LZW', '-co', 'NUM_THREADS=ALL_CPUS',
+                 '-ot', 'UInt16',
+                 '/data/inputs/polygons/green_belt.gpkg', outputs / 'green_belt_1m.tif'])
+
+subprocess.call(['gdalwarp',
+                 '-tr', '100', '100',
+                 '-r', 'sum',
+                 '-co', 'COMPRESS=LZW', '-co', 'NUM_THREADS=ALL_CPUS',
+                 '-ot', 'UInt16',
+                 '-overwrite',
+                 outputs / 'green_belt_1m.tif', outputs / 'green_belt_100m.tif'])
