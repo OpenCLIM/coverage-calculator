75334c71fb8b8d68bea85b624c8857ad700be823
James Virgo <47320567+JamesVirgo@users.noreply.github.com>
2021-06-08 18:11:27 +0100

    udm-rasterise-coverage TO coverage-calculator
    merging repos

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-## Coverage Calculator
+## UDM Rasterise Coverage
 
-Creates a GeoTIFF with coverage (m<sup>2</sup>) of polygons inside 100m<sup>2</sup> grid cells.
+Creates an ASCII raster with coverage in (m<sup>2</sup>) of polygons inside 100m<sup>2</sup> grid cells.
 
 A single GeoPackage (.gpkg) or Shapefile (.shp) containing polygons is required.
 
 `gdal_rasterize` is used to burn the value `1` into a 1m resolution raster.
-`gdalwarp` then resamples this raster to 100m resolution by summing cell values.
+`gdalwarp` resamples this raster to 100m resolution by summing cell values.
+'gdal_translate' converts to an ascii raster suitable for input to UDM. 
 
 ### Usage
-`docker build -t coverage-calculator . && docker run -v "data:/data" --name coverage-calculator coverage-calculator` 
+`docker build -t udm-rasterise-coverage . && docker run -v "data:/data" --name udm-rasterise-coverage udm-rasterise-coverage` 
--- script.py
+++ script.py
@@ -2,53 +2,103 @@ import subprocess
 from pathlib import Path
 import os
 import logging
+#import variables as v
 
-outputs = Path("/data/outputs")
-inputs = Path("/data/inputs")
+#------------------------------------------------------------------------------------------------------------------------------
+#rasterise_1m
 
+#configure directory/subdirectories
+#current_dir = str(Path.cwd())
+#print(current_dir)
+#inputs = Path(current_dir + '/data/inputs/')
+inputs = Path("/data/inputs/")
+#print(inputs)
+
+#temp = Path(current_dir + '/data/temp/')
+temp = Path("/data/temp/")
+#print(temp)
+
+#outputs = Path(current_dir + '/data/outputs/')
+outputs = Path("/data/outputs/")
+#print(outputs)
+
+temp.mkdir(exist_ok=True)
 outputs.mkdir(exist_ok=True)
 
-logger = logging.getLogger('coverage_calculator')
+#configure output
+logger = logging.getLogger('udm-rasterise-coverage')
 logger.setLevel(logging.INFO)
-fh = logging.FileHandler(outputs / 'coverage_calculator.log')
+fh = logging.FileHandler(outputs / 'udm-rasterise-coverage.log')
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
-input_files = []
+#get input polygons
+input_polygons = []
 for ext in ['shp', 'gpkg']:
-    input_files.extend(list(inputs.glob(f"*/*.{ext}")))
+    input_polygons.extend(list(inputs.glob(f"*/*.{ext}")))
 
-assert len(input_files) > 0, 'No input files found'
+assert len(input_polygons) > 0, 'No input polygons found'
+selected_polygons = input_polygons[0]
 
+#get extent 'xmin,ymin,xmax,ymax'
+#extent = '459000,202000,501000,244000'
 extent = os.getenv('EXTENT')
+
 if extent == 'None' or extent is None:
     extent = []
 else:
     extent = ['-te', *extent.split(',')]
 
-selected_file = input_files[0]
-logger.info(f'Rasterizing {selected_file}')
+#------------------------------------------------------------------------------------------------------------------------------
+#rasterise_1m
+
+logger.info(f'Rasterizing {selected_polygons}')
 
 subprocess.call(['gdal_rasterize',
-                 '-burn', '1',
-                 '-tr', '1', '1',
-                 '-co', 'COMPRESS=LZW', '-co', 'NUM_THREADS=ALL_CPUS',
-                 '-ot', 'UInt16',
-                 '-at',  # all pixels touched by polygons will be burned
-                 *extent,
-                 selected_file, outputs / 'coverage_1m.tif'])
+                 '-burn', '1',		#fixed value to burn for all objects
+                 '-tr', '1', '1',	#target resolution <xres> <yres>
+                 '-co', 'COMPRESS=LZW', '-co', 'NUM_THREADS=ALL_CPUS',	#creation options
+                 '-ot', 'UInt16',	#output data type
+                 '-at',  			#enable all-touched rasterisation
+                 *extent,			#'-te' <xmin> <ymin> <xmax> <ymax> 
+                 selected_polygons, temp / 'rasterise_1m.tif'])	#src_datasource, dst_filename
 
 logger.info('Rasterizing completed')
 
-logger.info('Upscaling raster')
+#------------------------------------------------------------------------------------------------------------------------------
+#sum_resample_100m
+
+logger.info('Sum resampling raster')
 
 subprocess.call(['gdalwarp',
-                 '-tr', '100', '100',
-                 '-r', 'sum',
-                 '-co', 'COMPRESS=LZW', '-co', 'NUM_THREADS=ALL_CPUS',
-                 '-ot', 'UInt16',
-                 '-overwrite',
-                 outputs / 'coverage_1m.tif', outputs / 'coverage_100m.tif'])
+                 '-tr', '100', '100',	#target resolution <xres> <yres>
+                 '-r', 'sum',			#resampling method e.g. sum OR mode
+                 '-co', 'COMPRESS=LZW', '-co', 'NUM_THREADS=ALL_CPUS',	#creation options
+                 '-ot', 'UInt16',		#output data type
+                 '-overwrite',			#overwrite target dataset if already exists
+                 temp / 'rasterise_1m.tif', temp / 'sum_resample_100m.tif'])	#srcfile, dstfile
 
-logger.info('Upscaling completed')
+logger.info('Sum resampling completed')
+
+#------------------------------------------------------------------------------------------------------------------------------
+#translate
+
+#get layer name
+#layer = 'test_region_coverage_100m.asc'
+layer = os.getenv('LAYER') + '_coverage_100m.asc'
+
+
+
+logger.info('Translating raster')
+
+subprocess.call(['gdal_translate',
+                 '-tr', '100', '100',	#target resolution <xres> <yres>                
+                 '-ot', 'UInt16',		#output data type    
+                 '-a_nodata', '0',		#set nodata value             
+                 temp / 'sum_resample_100m.tif', outputs / layer])	#srcfile, dstfile
+
+logger.info('Translating completed')
+
+#------------------------------------------------------------------------------------------------------------------------------
+
